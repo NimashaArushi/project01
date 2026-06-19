e880faf90afb6c43c3c559a341fc7578dcf463c0
arushi <arushinimasha@gmail.com>
2026-06-19 16:55:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'Arial' }}>
+      <h1 style={{ color: '#00d8ff' }}>Aarushi's First React App! 🚀</h1>
+      <p style={{ fontSize: '20px' }}>වැඩේ සුපිරියටම සාර්ථකයි! දැන් මගේම React App එකක් ලයිව් වැඩ.</p>
+    </div>
+  );
+}
+
+export default App;
