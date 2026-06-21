0f7b8170288304d62b8427b0ff63c405c530e3bb
arushi <arushinimasha@gmail.com>
2026-06-21 09:58:52 +0530

    Day 2:1: Completed Complete/Uncomplete Feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([
-    'Finish HTML/CSS Roadmaps 💻',
-    'Prepare for Java Exam 📚',
-    'Increase Green Squares on GitHub 🟩'
+    {text:'Finish HTML/CSS Roadmaps 💻',isCompleted:false},
+    {text:'Prepare for Java Exam 📚',isCompleted:false},
+    {text:'Increase Green Squares on GitHub 🟩',isCompleted:false}
   ]);
   const [inputValue, setInputValue] = useState('');
   
   const handleAddTask = () => {
     if (inputValue.trim() !== '') {
-      setTasks([...tasks, inputValue]);
+      setTasks([...tasks, {text: inputValue, isCompleted: false}]);
       setInputValue('');
     }
   };
@@ -19,6 +19,18 @@ function App() {
     const updatedTasks = tasks.filter((_, index) => index !== indexToDelete);
     setTasks(updatedTasks);
   };
+
+
+const handleToggleComplete=(indexToToggle)=>{
+const updatedTasks=tasks.map((task,index)=>{
+if(index===indexToToggle){
+  return{...task,isCompleted:!task.isCompleted};
+}
+return task;
+});
+setTasks(updatedTasks);
+}
+
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
@@ -42,12 +54,23 @@ function App() {
       <ul style={{ listStyleType: 'none', padding: 0, marginTop: '20px' }}>
         {tasks.map((task, index) => (
           <li key={index} style={{ background: '#f4f4f4', margin: '10px auto', padding: '10px', width: '350px', borderRadius: '5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <span>✅ {task}</span>
+      <span 
+  onClick={() => handleToggleComplete(index)} 
+  style={{
+    cursor: 'pointer',
+    textDecoration: task.isCompleted ? 'line-through' : 'none',
+    color: task.isCompleted ? 'gray' : 'black'
+  }}
+>
+  {task.isCompleted ? '⬛ ' : '✅ '} {task.text}
+</span>
+
+
             <button 
               onClick={() => handleDeleteTask(index)} 
               style={{ background: '#ff4d4d', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '3px', cursor: 'pointer' }}
             >
-              ❌
+              X
             </button>
           </li>
         ))}
